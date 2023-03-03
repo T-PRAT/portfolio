ebb0a18a427f5f6a213b173a24502a9b96d976d0
T-PRAT <Titouan.p@hotmail.fr>
2023-03-03 23:11:06 +0100

    dark almost

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className="">
       <Header />
-      <h1 className='text-creme'>Hello</h1>
-    </div>
+      <h1 className="">Hello</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero iusto quam illum laborum enim veritatis assumenda dicta amet a facilis et, molestias ullam provident, ea aliquam! Ab amet quis provident.</p>
+    </div >
   )
 }
 
